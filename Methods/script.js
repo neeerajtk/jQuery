3002b50a4844b6001e1f4fcc4ea2629001e4e289
neeerajtk <neerajtk560@gmail.com>
2020-03-25 11:06:47 +0530

    script modified

---
+++ Methods/script.js
@@ -0,0 +1,7 @@
+$("div").css("background","purple");
+$("div.highlight").css("width","200px");
+$("#third").css("border","4px solid orange");
+$("div:first-of-type").css("color","pink"); 
+// first-of-type is a css attribute to select only the first occurence
+// $("div:first").css("color","pink"); 
+// the above also works, but slower
